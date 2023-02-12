e051ef47000cb1eea3c38f3e4fc899ebb59639bc
Bharti <bharatifule10gmail.com>
2023-02-12 18:13:35 +0530

    Added: Mood Analyzer Problems

--- bridgelabz/moodAnalyzer/MoodAnalyzerMain.java
+++ bridgelabz/moodAnalyzer/MoodAnalyzerMain.java
@@ -3,17 +3,17 @@ package com.bridgelabz.moodAnalyzer;
 import java.util.Scanner;
 
 public class MoodAnalyzerMain {
-	 public static void main(String[] args) {
-	        Scanner scan = new Scanner(System.in);
-	        System.out.println("Enter the message");
-	        String message = scan.nextLine();
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the message");
+		String message = scan.nextLine();
 
-	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-	        moodAnalyzer.analyseMood(message);
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		moodAnalyzer.analyseMood(message);
 
-	        System.out.println("Enter the message");
-	        message = scan.nextLine();
+		System.out.println("Enter the message");
+		message = scan.nextLine();
 
-	        moodAnalyzer.analyseMood(message);
-	    }
+		moodAnalyzer.analyseMood(message);
+	}
 }
--- bridgelabz/moodTest/MoodAnalyzerTest.java
+++ bridgelabz/moodTest/MoodAnalyzerTest.java
@@ -1,26 +1,27 @@
 package com.bridgelabz.moodTest;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 
 import com.bridgelabz.moodAnalyzer.MoodAnalyzer;
 
 public class MoodAnalyzerTest {
-	 static MoodAnalyzer moodAnalyzer;
+	
+	static MoodAnalyzer moodAnalyzer;
 
-	    @Before
-	    static void init() {
-	        System.out.println("Before All");
-	        moodAnalyzer = new MoodAnalyzer();
-	    }
+	@BeforeAll
+	static void init() {
+		System.out.println("Before All");
+		moodAnalyzer = new MoodAnalyzer();
+	}
 
-	    @Test
-	    public void testAnalyseMood(){
-	        String result = moodAnalyzer.analyseMood("I am in Sad Mood");
-	        Assert.assertEquals(result,"SAD");
+	@Test
+	public void testAnalyseMood() {
+		String result = moodAnalyzer.analyseMood("I am in Sad Mood");
+		Assertions.assertEquals(result, "SAD");
 
-	        result = moodAnalyzer.analyseMood("In Happy mood");
-	        Assert.assertEquals(result,"HAPPY");
-	    }
+		result = moodAnalyzer.analyseMood("In Happy mood");
+		Assertions.assertEquals(result, "HAPPY");
+	}
 }
